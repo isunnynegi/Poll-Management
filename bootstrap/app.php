928a1f03f14ca3323c8e72ac01706deb1a7697fc
Pravesh <isunnynegi1992@gmail.com>
2026-04-17 08:35:19 +0530

    Url and IP issues

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,7 +13,7 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withBroadcasting(__DIR__.'/../routes/channels.php')
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        $middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
